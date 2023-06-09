c4d93d7609734d318325bdd9a4dddc685125398b
Haibara <13231188495@163.com>
2023-06-09 21:37:01 +0800

    更新项目进度 6.9：销售机会的管理

--- src/main/java/cn/edu/cqut/crmservice/controller/SaleChanceController.java
+++ src/main/java/cn/edu/cqut/crmservice/controller/SaleChanceController.java
@@ -5,10 +5,10 @@ import cn.edu.cqut.crmservice.service.ISaleChanceService;
 import cn.edu.cqut.crmservice.util.TableResult;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * <p>
@@ -32,5 +32,33 @@ public class SaleChanceController {
         Page<SaleChance> page1 = saleChanceService.page(saleChancePage);
         //getTotal返回表里的总记录数，getRecords返回当前页的数据列表
         return TableResult.ok("查询成功！", page1.getTotal(), page1.getRecords());
+    }
+
+    @PostMapping("/addSaleChance")//映射的地址与方法名没有关系
+    public TableResult<SaleChance> addContact(SaleChance saleChance) {
+        saleChance.setSalCreatTime(LocalDateTime.now());
+        saleChance.setSalState(1);
+        saleChanceService.save(saleChance);
+        return TableResult.ok("新增销售机会成功！");
+    }
+
+    @PostMapping("/updateSaleChance")
+    public TableResult<SaleChance> updateCustomer(SaleChance saleChance) {
+        saleChanceService.updateById(saleChance);
+        return TableResult.ok("修改销售机会成功！");
+    }
+
+    @PostMapping("/deleteSaleChance")//映射的地址与方法名没有关系
+    public TableResult<SaleChance> deleteCustomer(Integer[] ids) {//参数名要和前端的ajax方法中的data参数里面的属性名字一致
+        saleChanceService.removeByIds(Arrays.asList(ids));//asList用于将数组转化为List
+        return TableResult.ok("删除销售机会信息成功！");
+    }
+
+    @PostMapping("/assto")
+    public TableResult<SaleChance> assto(SaleChance saleChance) {
+        saleChance.setSalAssTime(LocalDateTime.now());
+        saleChance.setSalState(2);
+        saleChanceService.updateById(saleChance);
+        return TableResult.ok("指派成功！");
     }
 }
--- src/main/java/cn/edu/cqut/crmservice/entity/SaleChance.java
+++ src/main/java/cn/edu/cqut/crmservice/entity/SaleChance.java
@@ -3,6 +3,9 @@ package cn.edu.cqut.crmservice.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -68,6 +71,8 @@ public class SaleChance implements Serializable {
     /**
      * 创建时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime salCreatTime;
 
     /**
@@ -78,6 +83,8 @@ public class SaleChance implements Serializable {
     /**
      * 指派时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime salAssTime;
 
     /**
