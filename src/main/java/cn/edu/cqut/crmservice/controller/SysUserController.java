6963837aad04d25d64942f1d53542e9d35ea3d2c
Haibara <13231188495@163.com>
2023-06-11 09:58:52 +0800

    提交测试

--- src/main/java/cn/edu/cqut/crmservice/controller/SysUserController.java
+++ src/main/java/cn/edu/cqut/crmservice/controller/SysUserController.java
@@ -1,13 +1,18 @@
 package cn.edu.cqut.crmservice.controller;
 
+import cn.edu.cqut.crmservice.entity.SysUser;
+import cn.edu.cqut.crmservice.service.ISysUserService;
+import cn.edu.cqut.crmservice.util.JWTUtil;
+import cn.edu.cqut.crmservice.util.TableResult;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author CQUT
@@ -17,5 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sysUser")
 @CrossOrigin
 public class SysUserController {
+    @Autowired
+    private ISysUserService sysUserService;
+
+    private JWTUtil jwtUtil;
+
+    public TableResult<SysUser> login(SysUser sysUser) {
+        //查询多条件为等于关系
+        QueryWrapper<SysUser> wrapper = new QueryWrapper<>(sysUser);
+        SysUser user = sysUserService.getOne(wrapper);
+        if (user != null) {
+            String token = jwtUtil.createJwt(user);
+            return TableResult.ok(token, user);
+        } else {
+            return TableResult.error(1, "用户名或密码错误");
+        }
+    }
 
 }
