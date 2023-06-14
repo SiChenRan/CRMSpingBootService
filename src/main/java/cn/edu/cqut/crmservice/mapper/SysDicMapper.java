7512e4a367e35d30678ada05e15dbd4e4db3599f
246859 <2633565580@qq.com>
2023-06-14 17:41:01 +0800

    feat(api): 字典增删改查接口

---
+++ src/main/java/cn/edu/cqut/crmservice/mapper/SysDicMapper.java
@@ -0,0 +1,16 @@
+package cn.edu.cqut.crmservice.mapper;
+
+import cn.edu.cqut.crmservice.entity.SysDic;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Haibara
+ * @since 2023-06-14
+ */
+public interface SysDicMapper extends BaseMapper<SysDic> {
+
+}
---
+++ src/main/java/cn/edu/cqut/crmservice/service/ISysDicService.java
@@ -0,0 +1,16 @@
+package cn.edu.cqut.crmservice.service;
+
+import cn.edu.cqut.crmservice.entity.SysDic;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Haibara
+ * @since 2023-06-14
+ */
+public interface ISysDicService extends IService<SysDic> {
+
+}
